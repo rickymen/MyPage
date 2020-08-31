2b70a1a948ab4b0fec75f7ab4e4ef6b1de665fa9
Ricky Fernando <rickyfernandoacong@gmail.com>
2020-08-31 16:51:19 +0700

    change js script

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function goToLinkedin() {
-    window.open('https://www.linkedin.com/in/ricky-fernando-a04ab119b/', '_blank');
-  }
-  
+function alertGood() {
+  alert("Thank for the appreciation. Here's my linkedin account : https://www.linkedin.com/in/ricky-fernando-a04ab119b/");
+}
+
+function alertBad() {
+  alert("Please help me become better by sending me message via linkedin, here's my linkedin account : https://www.linkedin.com/in/ricky-fernando-a04ab119b/");
+}
